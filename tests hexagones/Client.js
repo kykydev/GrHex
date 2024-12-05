7f381369f366e70c727199d13910c2dba7a34a50
elouand <Elouandgamer@hotmail.com>
2024-12-05 14:06:35 +0100

    testHexagones

---
+++ tests hexagones/Client.js
@@ -0,0 +1,125 @@
+const bod = document.body
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    width=15
+    height=17
+    créerDamier(height,width,20)
+    console.log("drawing")
+
+    var terrain = []
+
+    for (var i=0;i<width*height;i++){
+        terrain.push("plaine")
+    }
+
+    console.log(terrain)
+    actualiserDamier(width,height,terrain)
+
+
+
+    function casesAdjacentes(pos,width,height){
+        console.log("pos:"+(Math.floor(pos/height)))
+        var adj = [pos];
+
+        //Utiliser mathfloor pour les /
+    return adj
+}
+
+
+
+
+    console.log(casesAdjacentes(238,width,height))
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+//-------------------Création Hexagone sous forme de tableau de points----------------------------------------
+function creerHexagone(rayon) {
+    var points = new Array();
+    for (var i = 0; i < 6; i++) {
+        var angle = i * Math.PI / 3;
+        var x = Math.sin(angle) * rayon + 40;
+        var y = -Math.cos(angle) * rayon + 40;
+        points.push([Math.round(x * 100) / 100, Math.round(y * 100) / 100]);
+    }
+    return points;
+}
+
+function créerDamier(nbColumns, nbLines, rayon) {
+    document.getElementById("jeu").style.visibility="visible";
+    document.getElementById("jeu").innerHTML = "";
+    Hexagone = creerHexagone(rayon);
+
+    for (var l = 0; l < nbLines; l++) {
+        for (var c = 0; c < nbColumns; c++) {
+            var d = "";
+            var x, y;
+
+            for (var i = 0; i < 6; i++) {
+                h = Hexagone[i];
+                x = h[0] + (Hexagone[1][0] - Hexagone[0][0]) * l * 2;
+                if (c % 2 == 1) {
+                    x += (Hexagone[1][0] - Hexagone[0][0]);
+                }
+                y = h[1] + (Hexagone[1][1] - Hexagone[0][1]) * c * 3;
+
+                if (i == 0) {
+                    d += "M" + x + "," + y + " L";
+                } else {
+                    d += x + "," + y + " ";
+                }
+            }
+            d += "Z";
+
+
+
+            d3.select("#jeu")
+                .append("path")
+                .attr("d", d)
+                .attr("stroke", "none")
+                .attr("fill", "aliceblue")
+                .attr("id", "h" + (l * nbColumns + c));
+
+
+
+        }
+    }
+}
+
+
+
+
+//Coloriage d'un hexagone
+function fill(id,couleur){
+    d3.select(("#h"+id)).attr("fill", couleur);
+}
+//Coloriage du damier
+
+function actualiserDamier(longueur,largeur,jeu){
+//document.getElementById("jeu").innerHTML+='<defs><pattern id="image" patternUnits="userSpaceOnUse" width="20" height="20"><image href="./HEX/prairie_1.jpg" x="0" y="0" width="20" height="20" /></pattern> </defs>'
+
+
+for (i=0;i<longueur*largeur;i++){
+    var color="aliceblue";
+if (jeu[i]=="eau"){color="DodgerBlue";}
+if (jeu[i]=="rocher"){color="darkgray"}
+if (jeu[i]=="montagne"){color="brown"}
+if (jeu[i]=="plaine"){color="lightgreen";}//lightgreen
+if (jeu[i]=="pasteque"){color ="lightcoral"}
+
+fill(i,color)
+}
+}
---
+++ tests hexagones/Game.js
@@ -0,0 +1,140 @@
+const crypto = require('crypto')
+const { Creature } = require('./Créature.js');
+class Game {
+    constructor(host) {
+        this.id = crypto.randomUUID()
+        this.host = host;
+        this.joueurs = [host];
+        this.nbJoueurs = 1;
+        this.joueursConnectes = 1;
+        this.nbtours = 0;
+        this.tourActuel = 0;
+        this.board = [];
+        this.terrain = [];
+        this.tanières = [];
+    }
+    addJoueur(joueur) {
+        this.joueurs.push(joueur);
+        this.joueursConnectes++;
+    }
+    removeJoueur(joueur) {
+        this.joueurs.splice(this.joueurs.indexOf(joueur), 1);
+        this.joueursConnectes--;
+    }
+    setNbJoueurs(nbJoueurs) {
+        this.nbJoueurs = nbJoueurs;
+    }
+    setTours(nbtours) {
+        this.nbtours = nbtours;
+    }
+    isFinished() {
+        return this.nbtours == this.tourActuel;
+    }
+
+    getAxis(pos){//Cette fonction renvoie sous forme d'un tableau de deux éléments les positions X et Y (donc la ligne et la colonne) de la case demandée. Utile pour jouer les tours
+        return [Math.floor(pos/13),pos%13]
+    }
+    getCase(axis){
+        return axis[0]*13+axis[1]
+    }
+
+    //Liste des cases adjacentes
+  casesAdjacentes(pos){
+    var adj = [pos];
+    if (pos/13!=0){//Si pas col gauche
+        adj.push(pos-13)
+    }
+    if (pos/13!=12){//Si pas col droite
+        adj.push(pos+13)
+    }
+
+    if (((pos%13)%2)==1){//ligne impaire
+    adj.push(pos+12);adj.push(pos+14);adj.push(pos-1);adj.push(pos+1)
+    }
+    else{//Ligne paire
+        if (pos%13!=0){//Ligne pas 0
+            adj.push(pos-1);
+            if (pos/13!=0){//Si pas col gauche
+                adj.push(pos-14);
+            }
+        }   if (pos%13!=12){//Ligne pas 12
+            adj.push(pos+1);
+            if (pos/13!=0){//Si pas col gauche
+                adj.push(pos-12);
+            }
+        }
+    }
+
+    //On enlève les incorrects
+    return adj.filter((element)=>element>=0&&element<this.board.length&&this.terrain[element]!="montagne");
+  }
+
+
+reproduction(){
+    var listeRepro;
+    for (var i=0;i<this.nbJoueurs;i++){
+        listeRepro = this.board[this.tanières[i]];
+        if (listeRepro.length<10&&(listeRepro!=[]||listeRepro!=undefined)){
+        for (var animal of listeRepro){
+            if (animal.cooldown==0){
+            for (var j of listeRepro){
+                if (j!=animal&&j.cooldown==0&&j.gender!=animal.gender){
+                    for (var y=0;y<animal.reproductionRate;y++){
+                        let creature;                           
+                        if (Math.random()*100<=50){
+                        creature = new Creature(animal.reproductionRate,animal.perception,animal.strength,"male",animal.tanière,animal.tanière)}
+                        else{
+                        creature = new Creature(animal.reproductionRate,animal.perception,animal.strength,"female",animal.tanière,animal.tanière)}
+
+                        this.board[animal.tanière].push(creature)
+                        this.joueurs[i].creatures.push(creature)
+                        animal.cooldown = 5;
+                        j.cooldown=5;
+                    }
+                }
+                }
+            }
+        }
+        }
+    }
+
+
+}
+
+
+
+getWinner(){
+    var trueTie = true;
+    for (var player of this.joueurs){
+        if (player.creatures.length>0){trueTie=false}
+    }
+    if (trueTie){return "Personne n'"}
+    var winner = this.joueurs[0];
+    for (var player of this.joueurs){
+        if (player.creatures.length>winner.creatures.length){
+            winner = player;
+        }
+        
+        if (player.getLastCreature()!=null && player.creatures.length==winner.creatures.length){
+            if (player.getLastCreature().dateOfBirth.getTime()>winner.getLastCreature().dateOfBirth.getTime()){
+                winner = player;
+            }
+        }
+
+    }
+    return winner.pseudo+" ";
+}
+    
+coco(position){
+    for (var test of this.casesAdjacentes(position)){
+        if (!this.tanières.includes(test)){
+        this.terrain[test]="eau";
+        if (this.board[test]!=0 && this.board[test].position!=this.board[test].tanière){this.board[test].tuer(this)};}
+    }
+
+    this.terrain[position]="montagne";
+    if (this.board[position]!=0){this.board[test].tuer(this)};
+}
+
+}
+module.exports = { Game };
