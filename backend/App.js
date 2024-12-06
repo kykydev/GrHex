beb125539d75847e35c6bee1eff7af8fb78f527a
elouand <Elouandgamer@hotmail.com>
2024-12-06 21:40:48 +0100

    push pour kyky

---
+++ backend/App.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { isNumberObject } = require('util/types');
+const server = http.createServer(app);
+const io = new require("socket.io")(server);
+
+app.use(express.static(__dirname));
+
+const PORT = 8888;
+const IP = "localhost"
+server.listen(PORT, () => {
+  console.log(`Server is running on ${IP}:${PORT}`);
+});
+
+
+
+app.get("/",(request,response)=>{
+    response.sendFile("index.html",{root:__dirname+"/../frontend"})
+})
