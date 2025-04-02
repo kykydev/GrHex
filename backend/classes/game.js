fdfe35d49280ce7bf7809c5b73c390432463be5d
elouand <Elouandgamer@hotmail.com>
2025-04-02 15:15:26 +0200

    map 'towers' fonctionnelle

--- backend/classes/game.js
+++ backend/classes/game.js
@@ -1503,7 +1503,13 @@ newTrade(data,idJoueur){
     if (joueur==undefined){return false}
     if (data.mesRessources==undefined || data.ville==undefined || data.ressourcesEnnemies==undefined || data.mesQuantites==undefined || data.quantitesEnnemies==undefined){return false}
     
+    if (data.mesRessources=="or"){
+        if (joueur.gold<data.mesQuantites){return false}
+    }
+    else{
     if (joueur.units[data.hdvStock]==undefined ||joueur.units[data.hdvStock][data.mesRessources]==undefined ||joueur.units[data.hdvStock][data.mesRessources]<data.mesQuantites){return false}
+    }
+    console.log("d")
     
     for (var z of Object.keys(this.players)){
         if (z.cite==data.ville){
