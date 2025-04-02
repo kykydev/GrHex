4d9dae27ee41efcb8838549f524988075e25b89e
elouand <Elouandgamer@hotmail.com>
2025-04-02 15:22:23 +0200

    fix: envoi de propositions d'échange

--- backend/classes/game.js
+++ backend/classes/game.js
@@ -1509,11 +1509,25 @@ newTrade(data,idJoueur){
     else{
     if (joueur.units[data.hdvStock]==undefined ||joueur.units[data.hdvStock][data.mesRessources]==undefined ||joueur.units[data.hdvStock][data.mesRessources]<data.mesQuantites){return false}
     }
-    console.log("d")
     
-    for (var z of Object.keys(this.players)){
+    for (var zk of Object.keys(this.players)){
+        var z = this.players[zk]
         if (z.cite==data.ville){
-            
+
+            var dist = 0
+   
+            var stratege1 = undefined
+            var stratege2 = undefined
+            for (var zz of Object.keys(joueur.units)){
+                if (joueur.units[zz].name=="Stratege"){stratege1=joueur.units[zz].position}
+            }
+            for (var zz of Object.keys(z.units)){
+                if (z.units[zz].name=="Stratege"){stratege2=z.units[zz].position}
+            }
+         
+
+
+            console.log("f"+dist)
             var trade = {
                 "envoyeur":joueur.idJoueur,
                 "ressourcesEnvoyées":data.mesRessources,
